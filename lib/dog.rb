972eef6d5ab120d4f64fa8809addf198adaf625d
Molly McCarron <mccarronmollye@gmail.com>
2019-01-11 15:54:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -68,7 +68,7 @@ class Dog
   
   def self.find_or_create_by(name:, breed:)
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
-    if !dog.empty? #if dog instance exists return that row 
+    if !dog.empty? #if dog instance exists we can play around with dog_data but it will not be saved to the database (avoiding dups!!)
       dog_data = dog[0]
       dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
     else 
